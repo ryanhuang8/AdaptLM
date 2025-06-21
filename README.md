59e24f53c126c22d076cde1b1fa052ee71f3f772
John Zou <john_zou@brown.edu>
2025-06-21 11:55:20 -0700

    feat: llm initializations, testing, readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ContextLLM
+
+
+### probelm statement:
+
+### Installation
+
+```
+python insteallation .a 
+```
+
+## LLMs 
+
+
+GPT-4o: for speed 
+Claude: code generation 
+Deepseek: for reasoning
+Hume: emotional 
+Gemini: research
+
+Vapi: voice convenient
+
+
+###
---
+++ src/llms/claude.py
@@ -0,0 +1,14 @@
+from llms.llm import BaseLLM
+
+class Claude(BaseLLM):
+    def __init__(self, model_name: str):
+        super().__init__(model_name)
+
+    def extract_context(self, prompt: str) -> str:
+        pass
+
+    def ingest_context(self, context_id: str, context: str) -> None:
+        pass    
+    
+    def generate_text(self, prompt: str) -> str:
+        pass
